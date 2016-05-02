658cf705373fb3e41807b007084e6990fc1f268b
Turki <makouk22@gmail.com>
2016-05-02 18:01:32 +0100

    first commit

---
+++ app/src/main/java/com/example/turki/quiz_final/MyApplication.java
@@ -0,0 +1,42 @@
+package com.example.turki.quiz_final;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by turki on 27/04/2016.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        printHashKey();
+    }
+    public void printHashKey(){
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.turki.quiz_final", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEY-HASH-:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+}
+}
